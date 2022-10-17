66ec5601550fd1c21eb9c96edc3b2ff36c090641
Nathan Burgess <nathans.email.address@gmail.com>
2022-10-16 22:04:01 -0700

    keyboard keys play sounds when pressed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 import * as Tone from 'tone';
+import { useCallback, useEffect } from 'react';
+
+
 
 function App() {
   const synth = new Tone.Synth().toDestination();
+  
+  const letterToNote = {
+    a: 'A4',
+    s: 'B4',
+    d: 'C4',
+    f: 'D4',
+    g: 'E4',
+    h: 'F4',
+    j: 'G4'
+  };
+  
+  const handleKeyPress = (e) => synth.triggerAttackRelease(letterToNote[e.key], '8n');
+  
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    // return () => document.removeEventListener('keydown', handleKeyPress);
+  }, []);
+  
 
 
   return (
@@ -16,3 +37,28 @@ function App() {
 }
 
 export default App;
+
+
+
+// export default function App() {
+//   // handle what happens on key press
+//   const handleKeyPress = useCallback((event) => {
+//     console.log(`Key pressed: ${event.key}`);
+//   }, []);
+
+//   useEffect(() => {
+//     // attach the event listener
+//     document.addEventListener('keydown', handleKeyPress);
+
+//     // remove the event listener
+//     return () => {
+//       document.removeEventListener('keydown', handleKeyPress);
+//     };
+//   }, [handleKeyPress]);
+
+//   return (
+//     <div>
+//       <h1>Hello world</h1>
+//     </div>
+//   );
+// }
